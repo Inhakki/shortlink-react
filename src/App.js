868b625b98e1d48e804ccb772f58e060f364fbe1
inhakki <inhakki@gmail.com>
2018-08-26 22:28:26 -0400

    first commit, added react router

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header>
+          <img src="/shorts.jpg"/>
+          <h1>Short shorts</h1>
+          <p>Url shortener</p>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
